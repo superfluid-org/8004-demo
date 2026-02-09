7aa5597b38b6d38b2d33fa8ad807808b5a01317f
Hubert <hubert@hubert.local>
2026-02-09 15:45:17 +0100

    fix: hide NaN balance in ConnectButton

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
               </span>
             </div>
           </div>
-          <ConnectButton />
+          <ConnectButton showBalance={false} />
         </div>
       </header>
 
